fc21751a266f75e45a522a921a628e4da9601bcc
Takumi0618 <takumi.mori.0618@gmail.com>
2021-02-07 11:00:50 +0900

    first commit

---
+++ src/reducks/users/operations.js
@@ -0,0 +1,13 @@
+import { signInAction } from './actions';
+import { push } from "connected-react-router";
+
+export const signIn = () => {
+  return async (dispatch, getState) => {
+    const state = getState();
+    const isSignedIn = state.users.isSignedIn;
+
+    if (!isSignedIn) {
+      
+    }
+  }
+}
